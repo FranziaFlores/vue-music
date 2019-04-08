d3237e3f78213e7bdf1368f0735ad3e43fde6d03
Franzia Flores <franzia@bunsan.io>
2019-04-08 13:02:18 -0600

    add project

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>PlatziMusic</h1>
+    <select v-model="selectedCountry">
+      <option v-for="country in countries" v-bind:value="country.value"> {{country.name}} </option>
+    </select>
+    <loader v-show="loading"></loader>
+    <ul>
+      <artist v-for="artist in artists" :artist="artist" :key="artist.mbid"></artist>
+    </ul>
+  </div>
+</template>
+
+<script>
+import getArtists from './api'
+import Artist from './components/Artist.vue'
+import Loader from './components/Loader.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        { name: 'Mexico', value: 'mexico' },
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Japan', value: 'japan' }
+      ],
+      selectedCountry: 'mexico',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Loader
+  },
+  methods: {
+    refreshArtists(){
+      const self = this
+      this.loading = true
+      this.artist = []
+      getArtists(this.selectedCountry)
+        .then(function (artists){
+          self.loading = false
+          self.artists = artists
+      })
+    }
+  },
+  mounted(){
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry( ){
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
